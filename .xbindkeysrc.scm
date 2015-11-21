b97e927c77a17c1e7f5a40dd41d60d8201c58f10
Vjekoslav Brajkovic <vjeko@brajkovic.org>
2015-11-20 16:40:16 -0800

    Glue logic.

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,3 @@
+(xbindkey '(Shift "b:4") "dbus-send --session --dest=org.gnome.Magnifier --type=method_call  --print-reply /org/gnome/Magnifier org.gnome.Magnifier.zoomIn")
+(xbindkey '(Shift "b:5") "dbus-send --session --dest=org.gnome.Magnifier --type=method_call  --print-reply /org/gnome/Magnifier org.gnome.Magnifier.zoomOut")
+
