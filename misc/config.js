e8ac04c790a1498c9213691d065125cd66c290e0
Vjeko Brajkovic <vjeko@brajkovic.org>
2019-01-07 17:59:52 -0800

    Update

--- misc/config.js
+++ misc/config.js
@@ -3,7 +3,7 @@
 /* The name of this package (not localized) */
 var PACKAGE_NAME = 'gnome-shell';
 /* The version of this package */
-var PACKAGE_VERSION = '3.30.1';
+var PACKAGE_VERSION = '3.30.2';
 /* 1 if gnome-bluetooth is available, 0 otherwise */
 var HAVE_BLUETOOTH = 1;
 /* 1 if networkmanager is available, 0 otherwise */
